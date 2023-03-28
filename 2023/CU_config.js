71eafdfc4e684c9bdb9ce7f72fe7de94e8087894
iTreffle <129120575+iTreffle@users.noreply.github.com>
2023-03-28 18:14:54 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023ontor",
+      "defaultValue": "2023qcmo",
       "required": "true",
       "disabled": "true"
     },
